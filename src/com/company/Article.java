13ae11ebef667cbdc0ddd0a0cc4f9ef5cb7437b6
Chinnapa <vortexkd@gmail.com>
2017-11-27 18:57:45 +0900

    綺麗にしました（多分）

--- src/com/company/Article.java
+++ src/com/company/Article.java
@@ -4,47 +4,63 @@ import org.jsoup.nodes.Element;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Comparator;
 import java.util.Date;
 
-public class Article implements Comparable<Article>{
-    private final String title;
-    private final String category;
-    private final String postTimeString;
-    private final Date postTime;
-    private final String url;
+public final class Article implements Comparable<Article>{
 
-    public Article(Element el,String baseUrl,String year) throws ParseException{
-        SimpleDateFormat d = new SimpleDateFormat("yyyy/MM/dd HH:mm");
-        this.title = el.getElementsByClass("thumb-s__tit").text();
-        this.url = Article.urlMaker(el.getElementsByClass("thumb-s__tit-link").attr("href").toString(),baseUrl);
-        this.category = el.getElementsByClass("thumb-s__category").text();
-        this.postTimeString = year+"/"+el.getElementsByClass("thumb-s__date").text();
-        this.postTime = d.parse(year+"/"+el.getElementsByClass("thumb-s__date").text());
+    private static final String DATE_PATTERN = "yyyy/MM/dd HH:mm";
+    private static final String ELEMENT_TITLE_CLASS = "thumb-s__tit";
+    private static final String ELEMENT_URL_CLASS = "thumb-s__tit-link";
+    private static final String ELEMENT_CATEGORY_CLASS = "thumb-s__category";
+    private static final String ELEMENT_DATE_CLASS = "thumb-s__date";
+    private static final String HREF = "href";
+    private static final String HTTP = "http";
+    private static final String DOT = ".";
+    private static final String FORWARD_SLASH =  "/";
 
-    }
 
-    @Override
-    public String toString() {
-        return "\""+this.title+"\",\""+this.category+"\",\""+this.postTimeString+"\",\""+this.url+"\"";
-    }
+    private final String TITLE;
+    private final String CATEGORY;
+    private final String POST_TIME_STRING;
+    private final Date POST_TIME;
+    private final String URL;
 
     static String urlMaker(String relativeLocation, String base){
-        if(relativeLocation=="" || relativeLocation==null){
+        if(relativeLocation.equals("") || relativeLocation == null){
             return "";
         }
-        if(relativeLocation.contains("http") || relativeLocation.contains(".")){
+        if(relativeLocation.contains(HTTP) || relativeLocation.contains(DOT)){
             return relativeLocation;
         }else {
             return base+relativeLocation;
         }
     }
 
-    public Date getPostTime() {
-        return postTime;
+    Article(Element el,String baseUrl,String year) throws ParseException{
+        SimpleDateFormat d = new SimpleDateFormat(DATE_PATTERN);
+        this.TITLE = el.getElementsByClass(ELEMENT_TITLE_CLASS).text();
+        this.URL = Article.urlMaker(el.getElementsByClass(ELEMENT_URL_CLASS).attr(HREF).toString(), baseUrl);
+        this.CATEGORY = el.getElementsByClass(ELEMENT_CATEGORY_CLASS).text();
+        this.POST_TIME_STRING = year + FORWARD_SLASH + el.getElementsByClass(ELEMENT_DATE_CLASS).text();
+        this.POST_TIME = d.parse(year + FORWARD_SLASH + el.getElementsByClass(ELEMENT_DATE_CLASS).text());
+
+    }
+
+    public Date getPOST_TIME() {
+        return POST_TIME;
     }
 
     @Override
+    public String toString() {
+        //ここもリトラル　を使わないんですか？ \" を変数にしてみたんですけど、もっとわかりづらいと思いました。。。
+        //return QUOTE + this.TITLE + QUOTE_COMMA_QUOTE + this.CATEGORY + QUOTE_COMMA_QUOTE ....
+        return "\"" +  this.TITLE + "\",\"" + this.CATEGORY + "\",\"" + this.POST_TIME_STRING + "\",\"" + this.URL + "\"";
+    }
+
+
+    @Override
     public int compareTo(Article other) {
-        return this.postTime.compareTo(other.getPostTime());
+        return this.POST_TIME.compareTo(other.getPOST_TIME());
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,7 +9,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class Main {
 
@@ -25,45 +26,52 @@ public class Main {
 
     private static void makeArticleListCsv(int intYear, int intMonth) throws IOException, InterruptedException, ParseException{
 
-        String month = String.format("%02d",intMonth);
-        String year = ""+intYear;
+        String month = String.format("%02d" , intMonth);
+        String year = String.valueOf(intYear);
         int page = 0;
-        String url = "http://news.mynavi.jp/list/headline/"+intYear+"/"+month+"/?page="+page;
-        String siteName = "http://"+url.split("/")[2];
+        String urlMain = "http://news.mynavi.jp/list/headline/" + intYear + "/" + month + "/?page=";
+        String siteName = "http://" + urlMain.split("/")[2];
+        String ELEMENT_CLASS = "thumb-s__item";
 
-        ArrayList<Article> articles = new ArrayList<>();
+        List<Article> articles = new ArrayList<>();
 
         while (true) {
-            page +=1;
-            url = "http://news.mynavi.jp/list/headline/"+intYear+"/"+month+"/?page="+page;
+            page += 1;
+            String url = urlMain + page;
 
             Document doc = Jsoup.connect(url).get();
-            Elements els = doc.getElementsByClass("thumb-s__item");
-            if (els.size()==0) {
+            Elements elements = doc.getElementsByClass(ELEMENT_CLASS);
+            if (elements.size() == 0) {
                 break;
             }
-            /*if (page > 15){ テストのために
-                break;
-            }*/
-            for (Element el : els) {
-                Article newArticle = new Article(el, siteName, year);
+            for (Element element : elements) {
+                Article newArticle = new Article(element, siteName, year);
                 if (!articles.contains(newArticle)) {
                     articles.add(newArticle);
                 }
             }
             Thread.sleep(1000);
         }
-        Collections.sort(articles);
+        //Collections.sort(articles); //List.sort()を利用してくださいと書かれてますが、static
+        Comparator<Article> c = (Article a1, Article a2) -> a1.compareTo(a2);
+        articles.sort(c);
+
         writeToCsv(articles);
     }
 
-    public static void writeToCsv(ArrayList<Article> articleList) throws IOException{
+    public static void writeToCsv(List<Article> articleList) throws IOException{
         String header = "件名,カテゴリ,ニュース日時,URL";
-        PrintWriter pw = new PrintWriter(System.getProperty("user.dir")+"/files/mynavi_articles.csv","UTF-8");
-        pw.write(header+"\n");
-        for (Article article:articleList) {
-            pw.write(article.toString()+"\n");
+        String filePath = System.getProperty("user.dir")+"/files/mynavi_articles.csv";
+        String utf8 = "UTF-8";
+        String lineSeparator = "line.separator";
+
+
+        PrintWriter pw = new PrintWriter(filePath,utf8);
+        pw.write(header + System.getProperty(lineSeparator));
+        for (Article article : articleList) {
+            pw.write(article.toString() + System.getProperty(lineSeparator));
         }
         pw.close();
+
     }
 }
